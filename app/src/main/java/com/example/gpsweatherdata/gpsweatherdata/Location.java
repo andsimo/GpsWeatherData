b395ec693b5720f0db861a43a4b5d40c5aad8e1f
Simon Andersson <andsimo@student.chalmers.se>
2015-02-04 17:21:59 +0100

    Det går ni att lägga in markers och circles. I dagsläget är dessa hårdkodade.

---
+++ app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/Location.java
@@ -0,0 +1,11 @@
+package com.example.gpsweatherdata.gpsweatherdata;
+
+/**
+ * Created by Simon on 2015-02-04.
+ */
+public class Location {
+
+    public Location(){
+
+    }
+}
--- app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/MapActivity.java
+++ app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/MapActivity.java
@@ -1,18 +1,31 @@
 package com.example.gpsweatherdata.gpsweatherdata;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends Activity {
 
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
 
@@ -37,4 +50,30 @@ public class MapActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onMapReady(GoogleMap gMap) {
+        map = gMap;
+        addMarker();
+    }
+
+    /*
+    Double lat och Double long inparametrar skall läggas till..!
+     */
+    public void addMarker(){
+        //3 st sätt att göra det på...
+        // 1. Marker med custom icon.
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+        //2. circle - Måste då tänka på radius!
+        map.addCircle(new CircleOptions().center(new LatLng(-2.037189, 15.075171)).radius(100000).strokeColor(Color.rgb(235, 231, 30)).fillColor(Color.YELLOW));
+
+
+        //3. Symbols... Har ännu ej testat.
+
+    }
+
+
 }
