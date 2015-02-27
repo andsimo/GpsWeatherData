4571735d38ae12bef4ac368681c7e14cc7a28df6
rijadalic <rijad.alic@telia.com>
2015-02-27 13:02:17 +0100

    Testvärden i listan

--- app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/Cities.java
+++ app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/Cities.java
@@ -45,35 +45,30 @@ public class Cities extends Activity {
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        // Adding child data    //Här ska sedan städers namn i det valda landet listas.
+        listDataHeader.add("Göteborg");
+        listDataHeader.add("Stockholm");
+        listDataHeader.add("Malmö");
+        listDataHeader.add("Västerås");
+        listDataHeader.add("Örebro");
+        listDataHeader.add("Mölndal");
+        listDataHeader.add("Umeå");
+        listDataHeader.add("Växjö");
+        listDataHeader.add("Kalmar");
 
-        // Adding child data
+
+
+        // Adding child data        //Gör om så att det skapas en view likt denna under varje stad där all info placeras.
         List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        top250.add("Installation date: 2014-02-01, S/V: 1.3, Sensor Value: Hej");
+
 
         List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        nowShowing.add("Installation date: 2013-05-20, S/V: 1.2, Sensor Value: Hej2");
 
         List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+        comingSoon.add("Installation date: 2014-11-10, S/V: 1.4, Sensor Value: Hej3");
+
 
         listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
         listDataChild.put(listDataHeader.get(1), nowShowing);
