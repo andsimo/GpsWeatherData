e2fae344a6429b452fa92ea0611f2a3e2d4aee43
rijadalic <rijad@student.chalmers.se>
2015-02-27 01:33:51 +0100

    Fixade en sista bild.

--- app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/Menu_Cont.java
+++ app/src/main/java/com/example/gpsweatherdata/gpsweatherdata/Menu_Cont.java
@@ -2,13 +2,25 @@ package com.example.gpsweatherdata.gpsweatherdata;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class Menu_Cont extends Activity{
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_cont);
+
+
     }
+
+
+
 }
